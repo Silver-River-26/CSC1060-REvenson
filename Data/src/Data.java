c15eaee9533df3e9e0a5e71aabb4656ae16eb709
Riven Evenson <Riven.Evenson@gmail.com>
2023-08-29 14:53:57 -0600

    class work

---
+++ Data/src/Data.java
@@ -0,0 +1,14 @@
+
+public class Data {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String day = "Tuesday";
+		int date = 29;
+		String month = "August";
+		int year = 2023;
+		System.out.println("American Format: " + day + ", " + month + " " + date + "," + " " + year);
+		System.out.println("European Format: " + day + ", " + date + " " + month + ", " + year);
+	}
+
+}
---
+++ Java Math/src/JavaMath.java
@@ -0,0 +1,17 @@
+
+public class JavaMath {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 5;
+		int num2 = 6;
+		int sum = num2 * num1;
+		System.out.println("The answer is " + sum);
+		System.out.print("hello!\nHow are you \"doing\"? \n");
+		System.out.println("Twinkle, twinkle, little star, \n\tHow I wonder what you are!\n\t\tUp above the world so high, \n\t\tLike a diamond in the sky.\nTwinkle, twinkle little star,\n\tHow I wonder what you are");
+		 
+		}
+	}
+	
+
+	
